2c26079442b445cead3cfce504eb572e39605258
박민수 <pinion7rl@gmail.com>
2022-05-11 00:06:57 +0900

    스프링 데이터 JPA가 제공하는 Querydsl 기능

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -2,11 +2,13 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberDto;
 import study.querydsl.dto.MemberSearchCondition;
@@ -21,16 +23,19 @@ import static org.springframework.util.StringUtils.hasText;
 import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
 
-public class MemberRepositoryImpl implements MemberRepositoryCustom {
+public class MemberRepositoryImpl extends QuerydslRepositorySupport implements MemberRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
+    // QuerydslRepositorySupport를 적용하려면 super()로 부모생성자를 받아와야함.
     public MemberRepositoryImpl(EntityManager em) {
+        super(Member.class);
         this.queryFactory = new JPAQueryFactory(em);
     }
 
     @Override
     public List<MemberTeamDto> search(MemberSearchCondition condition) {
+
         return queryFactory
                 .select(new QMemberTeamDto(
                         member.id.as("memberId"),
@@ -47,6 +52,26 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                         ageGoe(condition.getAgeGoe()),
                         ageLoe(condition.getAgeLoe())
                 )
+                .fetch();
+    }
+
+    public List<MemberTeamDto> search2(MemberSearchCondition condition) {
+
+        return from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
                 .fetch();
     }
 
@@ -78,13 +103,38 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
         return new PageImpl<>(content, pageable, totalCnt);
     }
 
+    public Page<MemberTeamDto> searchPageSimple2(MemberSearchCondition condition, Pageable pageable) {
+        JPQLQuery<MemberTeamDto> jpqlQuery = from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ));
+
+        QueryResults<MemberTeamDto> results = getQuerydsl().applyPagination(pageable, jpqlQuery).fetchResults();
+
+        List<MemberTeamDto> content = results.getResults();
+        long totalCnt = results.getTotal();
+
+        return new PageImpl<>(content, pageable, totalCnt);
+    }
+
     @Override
     public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
         List<MemberTeamDto> content = getMemberTeamDtos(condition, pageable); // 이렇게 따로 메서드를 만들어 리팩토링해도 좋음
 
         // 별도의 카운트 쿼리를 작성하면 더 최적화가 가능해서 효율적일 수 있음!
         // 위와 마찬가지로 따로 메서드를 만들어 리팩토링해도 좋음
-        JPAQuery<Member> countQuery = getCountQuery(condition);
+        JPAQuery<Long> countQuery = getCountQuery(condition);
 
 //        return new PageImpl<>(content, pageable, totalCnt);
         /**
@@ -93,7 +143,7 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
          *  - 페이지 시작이면서 컨텐츠 사이즈가 페이지 사이즈보다 작을 때
          *  - 마지막 페이지 일 때 (offset + 컨텐츠 사이즈를 더해서 전체 사이즈 구함)
          */
-        return PageableExecutionUtils.getPage(content, pageable, () -> countQuery.fetchCount());
+        return PageableExecutionUtils.getPage(content, pageable, () -> countQuery.fetchOne());
     }
 
     private List<MemberTeamDto> getMemberTeamDtos(MemberSearchCondition condition, Pageable pageable) {
@@ -121,9 +171,9 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
     }
 
 
-    private JPAQuery<Member> getCountQuery(MemberSearchCondition condition) {
-        JPAQuery<Member> countQuery = queryFactory
-                .select(member)
+    private JPAQuery<Long> getCountQuery(MemberSearchCondition condition) {
+        JPAQuery<Long> countQuery = queryFactory
+                .select(member.count())
                 .from(member)
                 .leftJoin(member.team, team)
                 .where(
--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -9,12 +9,14 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.member;
 
 @SpringBootTest
 @Transactional
@@ -115,4 +117,56 @@ class MemberRepositoryTest {
         assertThat(result.getSize()).isEqualTo(3);
         assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
     }
+
+    /**
+     * 1. QuerydslPredicateExecutor의 한계점
+     * 조인X (묵시적 조인은 가능하지만 left join이 불가능하다.)
+     * 클라이언트가 Querydsl에 의존해야 한다. 서비스 클래스가 Querydsl이라는 구현 기술에 의존해야 한다.
+     * 복잡한 실무환경에서 사용하기에는 한계가 명확하다.
+     * > 참고: QuerydslPredicateExecutor 는 Pageable, Sort를 모두 지원하고 정상 동작한다.
+     *
+     *
+     * 2. Querydsl Web 한계점 - 너무 쓸수가 없어서 샘플코드 그냥 안쓰고 추가로 설명하겠음
+     * 단순한 조건만 가능
+     * 조건을 커스텀하는 기능이 복잡하고 명시적이지 않음 컨트롤러가 Querydsl에 의존
+     * 복잡한 실무환경에서 사용하기에는 한계가 명확
+     *
+     *
+     * 3. 리포지토리 지원 - QuerydslRepositorySupport - 테스트 코드는 안만들었지만 MemberRepositoryImpl에 구현 코드는 있음. (참고하길)
+     * 1) 장점
+     * getQuerydsl().applyPagination() 스프링 데이터가 제공하는 페이징을 Querydsl로 편리하게 변환 가능(단! Sort는 오류발생)
+     * from() 으로 시작 가능(최근에는 QueryFactory를 사용해서 select() 로 시작하는 것이 더 명시적) EntityManager 제공
+     *
+     * 2) 한계
+     * Querydsl 3.x 버전을 대상으로 만듬
+     * Querydsl 4.x에 나온 JPAQueryFactory로 시작할 수 없음
+     * select로 시작할 수 없음 (from으로 시작해야함) QueryFactory 를 제공하지 않음
+     * 스프링 데이터 Sort 기능이 정상 동작하지 않음
+     */
+    @Test
+    void querydslPredicateExecutorTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3= new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        Iterable<Member> result = memberRepository.findAll(member.age.between(10, 40).and(member.username.eq("member1")));
+        for (Member member : result) {
+            System.out.println("member = " + member);
+        }
+    }
+
+    /**
+
+     */
+
 }
